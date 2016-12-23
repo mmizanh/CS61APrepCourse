26c3446d98438a7bfe04fa61fb45b011404f5692
mmizanh <munirahh@berkeley.edu>
2016-12-23 15:59:44 -0800

    Delete favorite-number.rb

--- favorite-number.rb
+++
@@ -1,4 +0,0 @@
-#Favorite number (interactive).
-puts 'What is your favorite number?'
-better = gets.to_i + 1
-puts 'Your favorite number is great, really, but I propose ' + better.to_s + ' as an alternative suggestion.'
