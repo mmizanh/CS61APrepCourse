a93dad7b03d630eff4af8da20bf6c48d7ab7f8b6
mmizanh <munirahh@berkeley.edu>
2016-12-27 20:22:16 -0800

    Complete Chapter 6 AFTTT

---
+++ Chapter_6/angry-boss.rb
@@ -0,0 +1,4 @@
+#Angry boss.
+
+puts 'What do you want?'
+puts 'WHADDAYA MEAN "' + gets.upcase.chomp + '"?!? YOU\'RE FIRED!!'
---
+++ Chapter_6/table-of-contents.rb
@@ -0,0 +1,7 @@
+#Table of contents.
+line_width = 45
+puts('Table of Contents'.center(line_width))
+puts(' '.center(line_width))
+puts('Chapter 1: Getting Started'.ljust(line_width/2) + 'page 1'.rjust(line_width/2))
+puts('Chapter 2: Numbers        '.ljust(line_width/2) + 'page 9'.rjust(line_width/2))
+puts('Chapter 3: Letters         '.ljust(line_width/2) + 'page 13'.rjust(line_width/2))
